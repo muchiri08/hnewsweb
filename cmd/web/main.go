613b1a0c78ad18b9e20ac906b442e5a3f3b5b30f
muchiri08 <mbogokennedy08@gmail.com>
2023-07-16 13:39:05 +0300

    config upper db, add static files in public folder

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/CloudyKit/jet/v6"
+	"github.com/alexedwards/scs/postgresstore"
 	"github.com/alexedwards/scs/v2"
+	_ "github.com/lib/pq"
+	"github.com/upper/db/v4"
+	"github.com/upper/db/v4/adapter/postgresql"
 	"log"
 	"net/http"
 	"os"
@@ -31,6 +36,28 @@ func main() {
 		port: "8009",
 		url:  "http://localhost:8080",
 	}
+
+	//init postgres db
+	datasourceName := "postgres://root:KiNuThiaPro$2@localhost/hackernews?sslmode=disable"
+	database, err := openDb(datasourceName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
+
+	//init upper/db
+	upper, err := postgresql.New(database)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(upper db.Session) {
+		err := upper.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(upper)
+
+	//init our application
 	app := application{
 		server:  server,
 		appName: "Hacker News",
@@ -54,8 +81,24 @@ func main() {
 	app.session.Cookie.Name = app.appName
 	app.session.Cookie.Domain = app.server.host
 	app.session.Cookie.SameSite = http.SameSiteStrictMode
+	app.session.Store = postgresstore.New(database)
 
+	//start the server
 	if err := app.listenAndServe(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func openDb(datasourceName string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", datasourceName)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
